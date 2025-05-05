99fe8a3d95f57ef78e197ba06f6cd4186bdfc713
amarkanta-naorem <amarkanta.meitei.naorem@gmail.com>
2025-05-05 21:04:07 +0530

    Dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#06c1db" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
